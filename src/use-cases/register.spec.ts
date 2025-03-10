96e1d20f674cbfb2978ac6721c0c9ad6ec53c244
Aluizio <aluizio.sln@gmail.com>
2025-03-10 17:56:30 -0400

    feat: add first unitary test

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,35 @@
-import {expect, test} from 'vitest'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { expect, describe, it} from 'vitest'
+import { RegisterUseCase } from '../use-cases/register'
+import { compare } from 'bcryptjs'
 
-test('check if it works', () => {
-    expect( 2 + 2 ).toBe(4)
+describe("Register Use Case", () => {
+    it('should hash the user password upon registration', async () => {
+        const registerUseCase = new RegisterUseCase({
+            async findByEmail(email){
+            return null
+        },
+            
+            async create(data){
+                return {
+                    id: 'user-1',
+                    name: data.name,
+                    email: data.email,
+                    password_hash: data.password_hash,
+                    createdAt: new Date(),
+                }
+            },
+    })
+
+        const { user } = await registerUseCase.execute({
+            name: 'John Doe',
+            email: 'johndoe@gmail.com',
+            password: '123456'    
+        })
+
+        const isPasswordCorrectlyHashed = await compare('123456', user.password_hash)
+
+        expect(isPasswordCorrectlyHashed).toBe(true)
+
+    })
 })
