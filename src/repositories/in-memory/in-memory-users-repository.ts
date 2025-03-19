715fdef98ebfde89f9d04938b2d7249434a03395
Aluizio <aluizio.sln@gmail.com>
2025-03-19 08:32:58 -0400

    feat: add get user profile use case/test/error

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -3,6 +3,17 @@ import { User, Prisma } from '@prisma/client';
 
 export class InMemoryUsersRepository implements UsersRepository {
     private items: User[] = []
+
+    async findById(id: string){
+        const user = this.items.find((item)=> item.id === id)
+        
+        if(!user){
+            return null
+        }
+
+        return user
+    }
+
     async findByEmail(email: string){
         const user = this.items.find((item)=> item.email === email)
         
